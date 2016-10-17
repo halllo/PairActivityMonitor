92265fb879cc288b6340dab0a0d4b0bf2336af70
Manuel Naujoks <mnaujoks@gmail.com>
2016-10-17 13:38:37 +0200

    mvvm

--- PairActivityMonitor/PairActivityMonitor/MainWindow.xaml.cs
+++ PairActivityMonitor/PairActivityMonitor/MainWindow.xaml.cs
@@ -1,33 +1,57 @@
-﻿using System.Windows;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
 using System.Windows.Interop;
 using KeyboardAndMouseEvents;
 
 namespace PairActivityMonitor
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
+	public partial class MainWindow
 	{
+		private readonly MainWindowModel _mainWindowModel;
 		public MainWindow()
 		{
 			InitializeComponent();
+			DataContext = _mainWindowModel = new MainWindowModel();
 
 			Loaded += (s, a) =>
 			{
-				var windowHandle = new WindowInteropHelper(Application.Current.MainWindow).Handle;
-				_rawInput = new RawInput(windowHandle);
-				_rawInput.KeyEvent += (sender, arg) => Log($"{_counter++}");
-				_rawInput.MouseEvent += (sender, arg) => Log($"{_counter++}");
+				_mainWindowModel.Start();
 			};
 		}
+	}
+
+
+
+
+
+
+
+
+	public class MainWindowModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
 
 		private RawInput _rawInput;
-		private decimal _counter = 0;
-
-		private void Log(string text)
+		public void Start()
 		{
-			textBlock.Text = text;
+			var windowHandle = new WindowInteropHelper(Application.Current.MainWindow).Handle;
+			_rawInput = new RawInput(windowHandle);
+			_rawInput.KeyEvent += (sender, arg) => EventCounter++;
+			_rawInput.MouseEvent += (sender, arg) => EventCounter++;
+		}
+
+
+		private int _eventCounter;
+		public int EventCounter
+		{
+			get { return _eventCounter; }
+			set { _eventCounter = value; OnPropertyChanged(); }
 		}
 	}
 }
