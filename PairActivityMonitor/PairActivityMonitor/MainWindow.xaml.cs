1a6696cf1b72ad59bf2c9dea98219ee7f3e8e3f9
Manuel Naujoks <mnaujoks@gmail.com>
2016-10-17 14:38:22 +0200

    metro style and red-green-red color gradient

--- PairActivityMonitor/PairActivityMonitor/MainWindow.xaml.cs
+++ PairActivityMonitor/PairActivityMonitor/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Interop;
+using System.Windows.Media;
 using KeyboardAndMouseEvents;
 
 namespace PairActivityMonitor
@@ -48,10 +49,44 @@ namespace PairActivityMonitor
 
 
 		private int _eventCounter;
+
 		public int EventCounter
 		{
 			get { return _eventCounter; }
 			set { _eventCounter = value; OnPropertyChanged(); }
+		}
+
+		private decimal _percentage;
+
+		public decimal Percentage
+		{
+			get { return _percentage; }
+			set
+			{
+				_percentage = value; OnPropertyChanged();
+				Background = FarbverlaufRotGrünRot(_percentage);
+			}
+		}
+
+
+		private Brush _background = Brushes.White;
+		public Brush Background
+		{
+			get { return _background; }
+			set { _background = value; OnPropertyChanged(); }
+		}
+		private Brush FarbverlaufRotGrünRot(decimal prozent)
+		{
+			var einProzent = 255 / 50.0m;
+
+			if (prozent <= 50.0m)
+			{
+				return new SolidColorBrush(Color.FromRgb((byte)(255 - prozent * einProzent), (byte)(0 + prozent * einProzent), 0));
+			}
+			else
+			{
+				return new SolidColorBrush(Color.FromRgb((byte)(0 + (prozent - 50) * einProzent), (byte)(255 - (prozent - 50) * einProzent), 0));
+			}
 		}
 	}
 }
